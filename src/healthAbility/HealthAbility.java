fb3c52009735c922bcb222c8ee370bd451e3de43
Jose Mora <quepereza.mora@gmail.com>
2014-03-04 20:01:59 -0600

    -Creación de paquete principal y clase principal.

---
+++ src/healthAbility/HealthAbility.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package healthAbility;
+
+/**
+ * @author jmora 4/3/14
+ *
+ */
+public class HealthAbility {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
