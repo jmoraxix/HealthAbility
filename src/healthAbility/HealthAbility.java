b83cd6c8be0d40e18f791d1ac1939b234410a980
Jose Mora <quepereza.mora@gmail.com>
2014-03-12 16:42:11 -0600

    - Actualizacion clase principal.

--- src/healthAbility/HealthAbility.java
+++ src/healthAbility/HealthAbility.java
@@ -1,20 +1,68 @@
 /**
+ * Primer Proyecto POO
+ * José David Mora Loría
+ * 2014004856
+ * Diego Delgado 
  * 
  */
 package healthAbility;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 /**
  * @author jmora 4/3/14
  *
  */
 public class HealthAbility {
+	
+	//Pantallas principales
+		
+	//Pantallas de menu
+		
+	//Pantallas emergentes
 
-	/**
-	 * @param args
-	 */
+	//Medico medicos[] = null ; 
+	private static final Dimension tamPantalla = Toolkit.getDefaultToolkit().getScreenSize();    
+	private final static Font letraTitulo1 = new Font("Georgia", Font.BOLD, 38);
+	private final static Font letraTexto1 = new Font("Georgia", Font.PLAIN, 28);
+	private final static Font letraTexto2 = new Font("Georgia", Font.PLAIN, 22);
+
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//LookAndFeel de la aplicación
+		JFrame.setDefaultLookAndFeelDecorated(true);
+	    try {
+	        UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+	    }
+	    catch (Exception e){
+        }
 
+//		login = new Login();
+//		login.setBounds(0, 0, tamTotal.width, tamTotal.height);
+//		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//      login.setVisible(true);
+
+	}
+
+	public static Dimension getTamPantalla() {
+		return tamPantalla;
+	}
+
+	public static Font getLetraTitulo1() {
+		return letraTitulo1;
+	}
+
+	public static Font getLetraTexto1() {
+		return letraTexto1;
+	}
+
+	public static Font getLetraTexto2() {
+		return letraTexto2;
 	}
 
 }
---
+++ src/healthAbility/InformacionProyecto.java
@@ -0,0 +1,90 @@
+/**
+ * José David Mora Loría
+ * 2014004856
+ * 12/03/2014
+ */
+package healthAbility;
+
+import healthAbility.vista.base.VentanaEmergente;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+/**
+ * @author jmora 12/3/14
+ *
+ */
+@SuppressWarnings("serial")
+public class InformacionProyecto extends VentanaEmergente 
+{
+	private final String VERSION = "VERSION 1.0.0 (Actualizado 12/3/14)";
+	
+	public InformacionProyecto() 
+	{
+		setBounds(300, 150, 817, 476);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel Proyecto = new JLabel("Health Ability");
+		Proyecto.setHorizontalAlignment(SwingConstants.CENTER);
+		Proyecto.setFont(new Font("Consolas", Font.BOLD, 18));
+		Proyecto.setBounds(10, 23, 763, 65);
+		getContentPane().add(Proyecto);
+		
+		JLabel Version = new JLabel(VERSION);
+		Version.setHorizontalAlignment(SwingConstants.CENTER);
+		Version.setFont(new Font("Consolas", Font.BOLD, 18));
+		Version.setBounds(171, 70, 450, 58);
+		getContentPane().add(Version);
+		
+		JLabel Colegio = new JLabel("Instituto Tecnológico de Costa Rica");
+		Colegio.setHorizontalAlignment(SwingConstants.CENTER);
+		Colegio.setFont(new Font("Consolas", Font.BOLD, 18));
+		Colegio.setBounds(151, 118, 443, 50);
+		getContentPane().add(Colegio);
+		
+		JLabel Expo12 = new JLabel("Ingeniería en Computación");
+		Expo12.setHorizontalAlignment(SwingConstants.CENTER);
+		Expo12.setFont(new Font("Consolas", Font.BOLD, 18));
+		Expo12.setBounds(151, 165, 451, 42);
+		getContentPane().add(Expo12);
+		
+		JLabel Departamento = new JLabel("Programación Orientada a Objetos");
+		Departamento.setHorizontalAlignment(SwingConstants.CENTER);
+		Departamento.setFont(new Font("Consolas", Font.BOLD, 18));
+		Departamento.setBounds(151, 205, 450, 42);
+		getContentPane().add(Departamento);
+		
+		JLabel Epecialiadad = new JLabel("Primer Proyecto");
+		Epecialiadad.setHorizontalAlignment(SwingConstants.CENTER);
+		Epecialiadad.setFont(new Font("Consolas", Font.BOLD, 18));
+		Epecialiadad.setBounds(152, 246, 450, 42);
+		getContentPane().add(Epecialiadad);
+		
+		JLabel lblJosDavidMora = new JLabel("Jose\u00C9 David Mora Lor\u00CDa");
+		lblJosDavidMora.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJosDavidMora.setFont(new Font("Consolas", Font.BOLD, 18));
+		lblJosDavidMora.setBounds(0, 299, 450, 42);
+		getContentPane().add(lblJosDavidMora);
+		
+		JLabel lblDiegoSolsPearanda = new JLabel("Diego Delgado");
+		lblDiegoSolsPearanda.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDiegoSolsPearanda.setFont(new Font("Consolas", Font.BOLD, 18));
+		lblDiegoSolsPearanda.setBounds(351, 299, 450, 42);
+		getContentPane().add(lblDiegoSolsPearanda);
+		
+		JLabel lblSeccind = new JLabel("2014");
+		lblSeccind.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSeccind.setFont(new Font("Consolas", Font.BOLD, 18));
+		lblSeccind.setBounds(161, 340, 450, 42);
+		getContentPane().add(lblSeccind);
+		
+//		JLabel label_1 = new JLabel("2012");
+//		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+//		label_1.setFont(new Font("Consolas", Font.BOLD, 18));
+//		label_1.setBounds(171, 366, 450, 42);
+//		getContentPane().add(label_1);
+	}
+}
