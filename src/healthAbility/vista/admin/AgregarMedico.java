c98581b98e57d41ea18821d7190e114b2fa3b8f2
José David Mora Loría <JosDavid@192.168.1.5>
2014-03-26 11:59:12 -0600

    - Modificacion ventana crear medico.

--- src/healthAbility/vista/admin/AgregarMedico.java
+++ src/healthAbility/vista/admin/AgregarMedico.java
@@ -9,16 +9,52 @@ package healthAbility.vista.admin;
 
 import healthAbility.vista.VentanaBase;
 
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 /**
  * @author JoséDavid 26/03/2014
  */
 public class AgregarMedico extends VentanaBase {
+	private JTextField txtNom;
+	private JTextField txtPrimAp;
+	private JTextField txtSecAp;
 
 	/**
 	 * Create the frame.
 	 */
 	public AgregarMedico() {
+		
+		JLabel lblAgregarMdico = new JLabel("Agregar m\u00E9dico");
+		lblAgregarMdico.setBounds(122, 172, 283, 39);
+		getContentPane().add(lblAgregarMdico);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(90, 246, 110, 32);
+		getContentPane().add(lblNombre);
+		
+		JLabel lblPrimerApellido = new JLabel("Primer apellido:");
+		lblPrimerApellido.setBounds(88, 289, 112, 32);
+		getContentPane().add(lblPrimerApellido);
+		
+		JLabel lblSegundoApellido = new JLabel("Segundo apellido:");
+		lblSegundoApellido.setBounds(88, 332, 112, 32);
+		getContentPane().add(lblSegundoApellido);
+		
+		txtNom = new JTextField();
+		txtNom.setBounds(208, 249, 160, 26);
+		getContentPane().add(txtNom);
+		txtNom.setColumns(10);
+		
+		txtPrimAp = new JTextField();
+		txtPrimAp.setBounds(210, 295, 160, 26);
+		getContentPane().add(txtPrimAp);
+		txtPrimAp.setColumns(10);
+		
+		txtSecAp = new JTextField();
+		txtSecAp.setBounds(209, 338, 159, 26);
+		getContentPane().add(txtSecAp);
+		txtSecAp.setColumns(10);
 
 	}
-
 }
